da96c2a77c0d697c4be44c3a7562443f255e60e0
Park Sung Jun <junstin119@gmail.com>
2024-06-20 23:21:24 +0900

    [feat] 이메일 서비스 - 랜덤 8자리 숫자 문자 메서드 생성

--- src/main/java/com/junstin/preorder/domain/email/service/EmailService.java
+++ src/main/java/com/junstin/preorder/domain/email/service/EmailService.java
@@ -4,10 +4,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
+
+
+
+    public static String generateRandomString(int length) {
+        final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        final int LENGTH = 8;
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+
+        return sb.toString();
+    }
+
 
 }
